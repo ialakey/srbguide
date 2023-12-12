ed8312f9e93e13d460f6d288ea15b0924de14b6a
alakey <prosoulk2017@gmail.com>
2023-12-12 18:29:44 +0100

    update design

--- lib/utils/snackbar_utils.dart
+++ lib/utils/snackbar_utils.dart
@@ -1,11 +1,11 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 class SnackbarUtils {
   static void showSnackbar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-      ),
-    );
+    Flushbar(
+      message: message,
+      duration: Duration(milliseconds: 850),
+    )..show(context);
   }
 }
--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -13,55 +13,50 @@ class DrawerScreen extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
-            decoration: BoxDecoration(
-              // color: Colors.blue,
-            ),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                  child: CircleAvatar(
+            decoration: BoxDecoration(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
                   radius: 40.0,
-                    backgroundImage: AssetImage('assets/serbia.png'),
+                  backgroundColor: Colors.transparent, // Убедитесь, что фон прозрачный
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/serbia.png',
+                      fit: BoxFit.cover, // Заполнение круга изображением
+                      width: 80.0, // Установка размера изображения
+                      height: 80.0,
+                    ),
                   ),
                 ),
-                  SizedBox(height: 10.0),
-                  Center(child: Text("Serbia Guide"))
-                ],
-              ),
+                SizedBox(height: 10.0),
+                Text("Serbia Guide", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              ],
             ),
           ),
-          ExpansionTile(
-          initiallyExpanded: true,
-          title: Text("Сервисы"),
-            leading: Icon(Icons.android),
-            children: [
-              ListTile(
-                leading: Icon(Icons.access_alarm),
-                title: Text('Калькулятор визарана'),
-                onTap: () {
-                  onNavItemTapped(0);
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.create),
-                title: Text('Создание белого картона'),
-                onTap: () {
-                  onNavItemTapped(1);
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.calculate),
-                title: Text('Калькулятор паушального налога'),
-                onTap: () {
-                  onNavItemTapped(2);
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+          ListTile(
+            leading: Icon(Icons.access_alarm),
+            title: Text('Калькулятор визарана'),
+            onTap: () {
+              onNavItemTapped(0);
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.create),
+            title: Text('Создание белого картона'),
+            onTap: () {
+              onNavItemTapped(1);
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.calculate),
+            title: Text('Калькулятор паушального налога'),
+            onTap: () {
+              onNavItemTapped(2);
+              Navigator.pop(context);
+            },
           ),
           ExpansionTile(
             leading: Icon(Icons.map),
@@ -80,10 +75,9 @@ class DrawerScreen extends StatelessWidget {
                 title: Text('Не курящие/Курящие'),
                 onTap: () {
                   UrlLauncherHelper.launchURL('https://lokalibezdima.rs');
-                  //onNavItemTapped(7);
                   Navigator.pop(context);
-                    },
-                  ),
+                },
+              ),
               ListTile(
                 leading: Icon(Icons.local_cafe),
                 title: Text('Русские заведения'),
@@ -92,8 +86,8 @@ class DrawerScreen extends StatelessWidget {
                   Navigator.pop(context);
                 },
               ),
-                ],
-              ),
+            ],
+          ),
           ExpansionTile(
             title: Text("База знаний"),
             leading: Icon(Icons.library_books),
